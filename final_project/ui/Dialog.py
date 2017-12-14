05a7cec9b07a6b0a2b44c420029afdcf9b038c3a
40623118 <40623118@gn.nfu.edu.tw>
2017-12-14 10:45:08 +0800

    final project 0~9 push button

--- final_project/ui/Dialog.py
+++ final_project/ui/Dialog.py
@@ -24,14 +24,21 @@ class Dialog(QDialog, Ui_Dialog):
         super(Dialog, self).__init__(parent)
         self.setupUi(self)
         '''以下為使用者自行編寫程式碼區'''
+        num = [self.one, self.two, self.three, self.four, \
+        self.five, self.six, self.seven, self.eight,\
+        self.nine, self.zero]
+        for i in num :
+            i.clicked.connect(self.digitClicked)
 
     def digitClicked(self):
+        
+        self.display.setText(self.display.text() + self.sender().text())
         '''
         使用者按下數字鍵, 必須能夠累積顯示該數字
         當顯示幕已經為 0, 再按零不會顯示 00, 而仍顯示 0 或 0.0
         
         '''
-        pass
+        #pass
         
     def unaryOperatorClicked(self):
         '''單一運算元按下後處理方法'''
